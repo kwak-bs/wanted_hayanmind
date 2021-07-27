bcccfae5249e66b7b166e0d3b2b2e0f1f119bc50
kwak-bs <iam-bs@naver.com>
2021-07-27 18:24:37 +0900

    Feat : Add infinite scroll This feature is currently not perfect.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import Comment from './component/Comment/Comment';
-import styled from 'styled-components';
+import React, { useState, useEffect} from "react";
+
+import Comment from "./component/Comment/Comment";
+import styled from "styled-components";
 
 export default function App() {
   const [data, setIsdata] = useState([]);
+  const [loading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/comments?_page=1&_limit=10', {
-      method: 'GET',
-    })
+  // data fetch
+  const getItems = () => {
+    setIsLoading(true);
+    fetch(
+      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=10`,
+      {
+        method: "GET",
+      }
+    )
       .then((res) => res.json())
-      .then((data) => setIsdata(data));
-  }, []);
+      .then((data) => {
+        setIsdata((prev) => prev.concat(data));
+        setIsLoading(false);
+      });
+  };
 
-  console.log(data);
+  // 초기 아이템 로딩
+  useEffect(() => {
+    getItems();
 
+    // 이슈 1. page 증가x, 최신 page를 못찾았던 이슈
+  }, [page]);
+  
+  // IntersectionObserver Callback
+  const onIntersect = ([entry], observer) => {
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target);
+      // 이슈 2. 무한 page 증식
+      setPage((prev) => prev + 1);
+      observer.observe(entry.target);
+    }
+  };
+
+  const [target, setTarget] = useState(null);
+  useEffect(() => {
+    let observer;
+    if (target) {
+      observer = new IntersectionObserver(onIntersect, { threshold: 0.5 });
+      observer.observe(target);
+    }
+    return () => observer && observer.disconnect();
+  }, [target]);
+
+  console.log(page);
   return (
     <Container>
       <Wrap>
         <Comment data={data} />
       </Wrap>
+      <div className="Loading" ref={setTarget}>
+        {loading && "Loading..."}
+      </div>
     </Container>
   );
 }
