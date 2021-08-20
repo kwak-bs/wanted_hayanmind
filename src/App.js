8f60feed30ea2038966100b9e5017bf4dfd84509
kwak-bs <iam-bs@naver.com>
2021-08-20 23:30:53 +0900

    FIX : comments route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from "react";
-import Comment from "./component/Comment/Comment";
+import Comment from "./Comment";
 import styled from "styled-components";
 
 export default function App() {
